e126d2264b882c21aa348001f47c49d5ba4efee9
uyuyorum {um} <56808589+alp1x@users.noreply.github.com>
2022-03-31 04:44:36 +0300

    Refactor

--- html/js/garage.js
+++ html/js/garage.js
@@ -1,42 +1,41 @@
-var Valetplate = "blabla"
+let Valetplate
 
-$(document).on('click', '.garage-vehicle', function(e){
+$(document).on('click', '.garage-vehicle', function(e) {
     e.preventDefault();
 
     $(".garage-homescreen").animate({
-        left: 30+"vh"
+        left: 30 + "vh"
     }, 200);
     $(".garage-detailscreen").animate({
-        left: 0+"vh"
+        left: 0 + "vh"
     }, 200);
 
     var Id = $(this).attr('id');
-    var VehData = $("#"+Id).data('VehicleData');
-    SetupDetails(VehData);  
-	Valetplate = (VehData.plate)
+    var VehData = $("#" + Id).data('VehicleData');
+    SetupDetails(VehData);
+    Valetplate = (VehData.plate)
 });
 
-$(document).on('click', '#garageback', function(e){
+$(document).on('click', '#garageback', function(e) {
     e.preventDefault();
 
     $(".garage-homescreen").animate({
-        left: 00+"vh"
+        left: 00 + "vh"
     }, 200);
     $(".garage-detailscreen").animate({
-        left: -30+"vh"
+        left: -30 + "vh"
     }, 200);
 });
 
-$(document).on('click', '#garagevalet', function(e){
+$(document).on('click', '#garagevalet', function(e) {
     e.preventDefault();
-	SetupValetCars(Valetplate);
+    SetupValetCars(Valetplate);
 });
 
 SetupValetCars = function(data) {
-	console.log(data)
-	if ((Valetplate !== "")) {
-		$.post('https://qb-phone/GetCar', JSON.stringify({
-			profilepicture: Valetplate,
-		}));
-	}
+    if ((Valetplate !== "")) {
+        $.post('https://qb-phone/GetCar', JSON.stringify({
+            carplate: Valetplate,
+        }));
+    }
 }
